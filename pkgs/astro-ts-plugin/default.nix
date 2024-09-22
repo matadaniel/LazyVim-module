43818aabb5a27a4bc0c5aeb3c3b6100567ee039a
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-21 22:37:30 -0500

    Create astro-ts-plugin

---
+++ pkgs/astro-ts-plugin/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pnpm_8,
+  nodejs_22,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "astro-ts-plugin";
+  version = "1.8.0";
+
+  src = fetchFromGitHub {
+    owner = "withastro";
+    repo = "language-tools";
+    rev = "@astrojs/ts-plugin@${finalAttrs.version}";
+    hash = "sha256-WdeQQaC9AVHT+/pXLzaC6MZ6ddHsFSpxoDPHqWvqmiQ=";
+  };
+
+  pnpmDeps = pnpm_8.fetchDeps {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      pnpmWorkspace
+      prePnpmInstall
+      ;
+    hash = "sha256-BiTrG5sfQmRQEmvZng+OxyUnuLH+rVe827BnICU9FPE=";
+  };
+
+  nativeBuildInputs = [
+    nodejs_22
+    pnpm_8.configHook
+  ];
+
+  buildInputs = [ nodejs_22 ];
+
+  pnpmWorkspace = "@astrojs/ts-plugin";
+  prePnpmInstall = ''
+    pnpm config set dedupe-peer-dependents false
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm --filter=@astrojs/ts-plugin build
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir $out
+    cp -r {packages/ts-plugin/{dist,LICENSE,package.json,README.md},node_modules} $out
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A TypeScript Plugin providing Astro intellisense";
+    homepage = "https://github.com/withastro/language-tools";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.unix;
+  };
+})

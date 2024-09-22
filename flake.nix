4198a6947d29f082af67dc2a005b52ca650f49a1
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-21 22:21:50 -0500

    Create flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Home Manager module for LazyVim";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs =
+    { self, ... }:
+    {
+      homeManagerModules = {
+        default = self.homeManagerModules.lazyvim;
+        lazyvim = import ./neovim self;
+      };
+    };
+}

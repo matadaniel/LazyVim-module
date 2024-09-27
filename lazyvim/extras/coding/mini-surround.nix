ac366ce7f74555eb657a159b855e5a3470cff282
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-27 16:46:30 -0500

    Update to mini-surround

--- lazyvim/extras/coding/mini-surround.nix
+++ lazyvim/extras/coding/mini-surround.nix
@@ -19,13 +19,7 @@ in
 
   config = mkIf cfg.extras.coding.mini-surround.enable {
     programs.neovim = {
-      plugins = [
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "mini.surround";
-          version = "2024-06-23";
-          src = inputs.mini-surround;
-        })
-      ];
+      plugins = [ pkgs.vimPlugins.mini-surround ];
     };
   };
 }

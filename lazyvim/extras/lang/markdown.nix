0007003c9e74d0107b03ab1188b4bacc68778cbc
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-09-27 17:03:14 -0500

    Update to render-markdown-nvim

--- lazyvim/extras/lang/markdown.nix
+++ lazyvim/extras/lang/markdown.nix
@@ -21,14 +21,9 @@ in
     programs.neovim = {
       extraPackages = builtins.attrValues { inherit (pkgs) markdown-toc markdownlint-cli2 marksman; };
 
-      plugins = [
-        pkgs.vimPlugins.markdown-preview-nvim
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "markdown.nvim";
-          version = "2024-08-09";
-          src = inputs.markdown-nvim;
-        })
-      ];
+      plugins = builtins.attrValues {
+        inherit (pkgs.vimPlugins) markdown-preview-nvim render-markdown-nvim;
+      };
     };
   };
 }

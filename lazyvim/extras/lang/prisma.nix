77326ab3acf1bb351e59d3ed58aea83793f94b6a
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-11-22 19:23:30 -0600

    Add prisma extra

---
+++ lazyvim/extras/lang/prisma.nix
@@ -0,0 +1,26 @@
+self:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.programs.lazyvim;
+in
+{
+  options.programs.lazyvim.extras.lang.prisma = {
+    enable = mkEnableOption "the lang.prisma extra";
+  };
+
+  config = mkIf cfg.extras.lang.prisma.enable {
+    programs.neovim = {
+      extraPackages = [ pkgs.nodePackages."@prisma/language-server" ];
+
+      plugins = [ (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [ plugins.prisma ])) ];
+    };
+  };
+}

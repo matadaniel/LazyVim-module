fe1f398ea2d3e81575e067bbcbed0c31bc270aaf
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-12-09 18:05:32 -0600

    Create dap.core extra

---
+++ lazyvim/extras/dap/core.nix
@@ -0,0 +1,31 @@
+self:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.programs.lazyvim;
+in
+{
+  options.programs.lazyvim.extras.dap.core = {
+    enable = mkEnableOption "the dap.core extra";
+  };
+
+  config = mkIf cfg.extras.dap.core.enable {
+    programs.neovim = {
+      plugins = builtins.attrValues {
+        inherit (pkgs.vimPlugins)
+          nvim-dap
+          nvim-dap-ui
+          nvim-dap-virtual-text
+          nvim-nio
+          ;
+      };
+    };
+  };
+}

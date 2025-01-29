67cfe162c364d3b9afec80befd037afdffec471f
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2025-01-29 10:37:50 +0000

    Add lang.rust

---
+++ lazyvim/extras/lang/rust.nix
@@ -0,0 +1,31 @@
+self:
+{
+	config,
+	lib,
+	pkgs,
+	...
+}:
+let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.programs.lazyvim;
+in
+{
+  options.programs.lazyvim.extras.lang.rust = {
+    enable = mkEnableOption "the lang.rust extra";
+  };
+
+  config = mkIf cfg.extras.lang.rust.enable {
+    programs.neovim = {
+      extraPackages = [ pkgs.rust-analyzer ];
+
+      plugins = [
+        (pkgs.vimPlugins.nvim-treesitter.withPlugins (
+          plugins: builtins.attrValues { inherit (plugins) rust; }
+        ))
+      ] ++ (with pkgs.vimPlugins; [ crates-nvim rustaceanvim ]);
+    };
+  };
+}
+

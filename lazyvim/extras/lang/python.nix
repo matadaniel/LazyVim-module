8e4d3e2b3f5980be5791cbaf53e18f2ff148a14d
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-20 18:26:00 -0500

    fix(python): use venv-selector main branch
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- lazyvim/extras/lang/python.nix
+++ lazyvim/extras/lang/python.nix
@@ -35,28 +35,28 @@ in
               rst;
           }
         ))
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "venv-selector.nvim";
-          version = "2024-09-15";
-          src = pkgs.fetchFromGitHub {
-            owner = "linux-cultist";
-            repo = "venv-selector.nvim";
-            rev = "e82594274bf7b54387f9a2abe65f74909ac66e97";
-            sha256 = "0d2lzx5b1jc0zq92ziy83apxak6b2rjsgi8nz6jwyy58l96lhb03";
-          };
-          meta.homepage = "https://github.com/linux-cultist/venv-selector.nvim/";
-        })
         # (pkgs.vimUtils.buildVimPlugin {
         #   pname = "venv-selector.nvim";
-        #   version = "2024-11-23";
+        #   version = "2024-09-15";
         #   src = pkgs.fetchFromGitHub {
         #     owner = "linux-cultist";
         #     repo = "venv-selector.nvim";
-        #     rev = "f212a424fb29949cb5e683928bdd4038bbe0062d";
-        #     sha256 = "1m3nlxi7aghm8bnbh7vh3h47yz21lj4higywwvq1s8xr5bizb0ig";
+        #     rev = "e82594274bf7b54387f9a2abe65f74909ac66e97";
+        #     sha256 = "0d2lzx5b1jc0zq92ziy83apxak6b2rjsgi8nz6jwyy58l96lhb03";
         #   };
         #   meta.homepage = "https://github.com/linux-cultist/venv-selector.nvim/";
         # })
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "venv-selector.nvim";
+          version = "2024-11-23";
+          src = pkgs.fetchFromGitHub {
+            owner = "linux-cultist";
+            repo = "venv-selector.nvim";
+            rev = "f212a424fb29949cb5e683928bdd4038bbe0062d";
+            sha256 = "1m3nlxi7aghm8bnbh7vh3h47yz21lj4higywwvq1s8xr5bizb0ig";
+          };
+          meta.homepage = "https://github.com/linux-cultist/venv-selector.nvim/";
+        })
         neotest-python
         nvim-dap-python
       ];

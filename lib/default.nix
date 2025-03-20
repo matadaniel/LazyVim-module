6d15bca077976971bd79861cbd41962bd60c2179
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-20 13:35:36 -0500

    Add strings to lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,11 @@
 let
   plugins = import ../lazyvim/plugins/lib;
+  strings = import ./strings.nix;
 in
 {
   inherit (plugins) buildVimPlugin buildVimPlugins;
+  inherit (strings) formatList hasNvimVersion;
 
   inherit plugins;
+  inherit strings;
 }

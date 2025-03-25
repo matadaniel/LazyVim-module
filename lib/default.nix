8200e97632169cfccb2f23206483ed2d6ca91d29
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-25 12:35:02 -0500

    Add generators to lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,12 +1,15 @@
 { lib }:
 let
+  generators = import ./generators.nix { inherit lib; };
   plugins = import ../lazyvim/plugins/lib;
   strings = import ./strings.nix { inherit lib; };
 in
 {
+  inherit (generators) toLazySpecs;
   inherit (plugins) buildVimPlugin buildVimPlugins;
   inherit (strings) formatList hasNvimVersion;
 
+  inherit generators;
   inherit plugins;
   inherit strings;
 }

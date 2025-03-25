f749fcec08c74590eb49259997c70160faa79fa0
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-25 12:36:46 -0500

    Create options lib

---
+++ lib/options.nix
@@ -0,0 +1,15 @@
+{ lib }:
+let
+  inherit (lib.options) mkOption;
+  inherit (lib.types) bool;
+
+  set = {
+    mkNotExtraOption = mkOption {
+      default = false;
+      internal = true;
+      type = bool;
+      visible = false;
+    };
+  };
+in
+set

46bd3a6fd863952e350ace8afa39d7ba8cafe324
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-25 13:58:07 -0500

    Change lang.astro specs to lazySpecs

--- lazyvim/extras/lang/astro.nix
+++ lazyvim/extras/lang/astro.nix
@@ -6,6 +6,7 @@ self:
   ...
 }:
 let
+  inherit (lib.generators) mkLuaInline;
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption;
 
@@ -17,6 +18,62 @@ in
   };
 
   config = mkIf cfg.extras.lang.astro.enable {
+    programs.lazyvim = {
+      lazySpecs = {
+        extras.lang.astro = [
+          {
+            localVars = true;
+
+            tsservers = [
+              "/tsserverlibrary.js"
+              "/typescript.js"
+              "/tsserver.js"
+            ];
+          }
+          {
+            ref = "neovim/nvim-lspconfig";
+            opts.servers.astro.on_new_config =
+              let
+                tsdk = "${pkgs.vtsls}/lib/vtsls-language-server/node_modules/typescript/lib";
+              in
+              mkLuaInline
+                # lua
+                ''
+                  function(new_config, new_root_dir)
+                    if new_root_dir then
+                      local tsdk = new_root_dir .. "/node_modules/typescript/lib"
+                      for i = 1, #tsservers do
+                        local serverPath = tsdk .. tsservers[i]
+                        if vim.uv.fs_stat(serverPath) then
+                          new_config.init_options.typescript.serverPath = serverPath
+                          new_config.init_options.typescript.tsdk = tsdk
+                          return
+                        end
+                      end
+                    end
+
+                    local tsdk = "${tsdk}"
+                    for i = 1, #tsservers do
+                      local serverPath = tsdk .. tsservers[i]
+                      if vim.uv.fs_stat(serverPath) then
+                        new_config.init_options.typescript.serverPath = serverPath
+                        new_config.init_options.typescript.tsdk = tsdk
+                        return
+                      end
+                    end
+
+                    LazyVim.error(
+                      "Failed to find vendored Typescript module in ${tsdk}",
+                      { title = "LazyVim-module" }
+                    )
+                    new_config.init_options.typescript.serverPath = nil
+                    new_config.init_options.typescript.tsdk = nil
+                  end'';
+          }
+        ];
+      };
+    };
+
     programs.neovim = {
       extraPackages = [ pkgs.astro-language-server ];
 
@@ -26,60 +83,5 @@ in
         ))
       ];
     };
-
-    xdg.configFile."nvim/lua/plugins/astro.lua".text =
-      let
-        tsdk = "${pkgs.vtsls}/lib/vtsls-language-server/node_modules/typescript/lib";
-      in
-      # lua
-      ''
-        local tsservers = {
-        	"/tsserverlibrary.js",
-        	"/typescript.js",
-        	"/tsserver.js",
-        }
-
-        return {
-        	{
-        		"neovim/nvim-lspconfig",
-        		opts = {
-        			servers = {
-        				astro = {
-        					on_new_config = function(new_config, new_root_dir)
-        						if new_root_dir then
-        							local tsdk = new_root_dir .. "/node_modules/typescript/lib"
-        							for i = 1, #tsservers do
-        								local serverPath = tsdk .. tsservers[i]
-        								if vim.uv.fs_stat(serverPath) then
-        									new_config.init_options.typescript.serverPath = serverPath
-        									new_config.init_options.typescript.tsdk = tsdk
-        									return
-        								end
-        							end
-        						end
-
-        						local tsdk = "${tsdk}"
-        						for i = 1, #tsservers do
-        							local serverPath = tsdk .. tsservers[i]
-        							if vim.uv.fs_stat(serverPath) then
-        								new_config.init_options.typescript.serverPath = serverPath
-        								new_config.init_options.typescript.tsdk = tsdk
-        								return
-        							end
-        						end
-
-        						LazyVim.error(
-        							"Failed to find vendored Typescript module in ${tsdk}",
-        							{ title = "LazyVim-module" }
-        						)
-        						new_config.init_options.typescript.serverPath = nil
-        						new_config.init_options.typescript.tsdk = nil
-        					end,
-        				},
-        			},
-        		},
-        	},
-        }
-      '';
   };
 }

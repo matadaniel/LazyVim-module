cc24e2a34b452bd7904a9e3663290dbad455216a
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-25 12:39:04 -0500

    Add types to lib

--- lib/default.nix
+++ lib/default.nix
@@ -4,14 +4,17 @@ let
   options = import ./options.nix { inherit lib; };
   plugins = import ../lazyvim/plugins/lib;
   strings = import ./strings.nix { inherit lib; };
+  types = import ./types.nix { inherit lib; };
 in
 {
   inherit (generators) toLazySpecs;
   inherit (plugins) buildVimPlugin buildVimPlugins;
   inherit (strings) formatList hasNvimVersion;
+  inherit (types) nested;
 
   inherit generators;
   inherit options;
   inherit plugins;
   inherit strings;
+  inherit types;
 }

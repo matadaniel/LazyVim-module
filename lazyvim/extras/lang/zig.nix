20664c71181b586199150a925c0be8e1fd2918f1
Jonathan Boyle <j.n.boyle@reading.ac.uk>
2025-03-26 09:31:25 +0000

    Fix for optional

--- lazyvim/extras/lang/zig.nix
+++ lazyvim/extras/lang/zig.nix
@@ -1,25 +1,28 @@
-self: {
+self:
+{
   config,
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption;
 
   cfg = config.programs.lazyvim;
-in {
+in
+{
   options.programs.lazyvim.extras.lang.zig = {
     enable = mkEnableOption "the lang.zig extra";
   };
 
   config = mkIf cfg.extras.lang.zig.enable {
     programs.neovim = {
-      extraPackages = builtins.attrValues {inherit (pkgs) zls;};
+      extraPackages = builtins.attrValues { inherit (pkgs) zls; };
 
       plugins = [
         (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [ plugins.zig ]))
-      ] ++ optional cfg.extras.test.core.enable pkgs.vimPlugins.neotest-zig;
+      ] ++ lib.optional cfg.extras.test.core.enable pkgs.vimPlugins.neotest-zig;
     };
   };
 }

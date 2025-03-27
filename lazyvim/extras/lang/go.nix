3ced13b590f748c742a3a21d81c133f0a1fd7e34
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2025-03-27 15:25:49 -0500

    Create lang.go extra

---
+++ lazyvim/extras/lang/go.nix
@@ -0,0 +1,48 @@
+self:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (builtins) attrValues;
+  inherit (lib.lists) optional;
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.programs.lazyvim;
+in
+{
+  options.programs.lazyvim.extras.lang.go = {
+    enable = mkEnableOption "the lang.go extra";
+  };
+
+  config = mkIf cfg.extras.lang.go.enable {
+    programs.neovim = {
+      extraPackages =
+        attrValues { inherit (pkgs) gofumpt gopls gotools; }
+        ++ optional cfg.extras.dap.core.enable pkgs.delve;
+      # TODO: ++ optionals cfg.extras.lsp.none-ls.enable (attrValues {
+      #   inherit (pkgs) gomodifytags impl;
+      # });
+
+      plugins =
+        [
+          (pkgs.vimPlugins.nvim-treesitter.withPlugins (
+            plugins:
+            attrValues {
+              inherit (plugins)
+                go
+                gomod
+                gosum
+                gowork
+                ;
+            }
+          ))
+        ]
+        ++ optional cfg.extras.dap.core.enable pkgs.vimPlugins.nvim-dap-go
+        ++ optional cfg.extras.test.core.enable pkgs.vimPlugins.neotest-golang;
+    };
+  };
+}

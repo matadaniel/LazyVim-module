48a1384fe9e34f4f458cbcf459346b6b117243ad
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-03-29 00:45:55 +0700

    Remove nixfmt-rfc-style (#17)
    LazyVim does not install a nixfmt package.

--- lazyvim/extras/lang/nix.nix
+++ lazyvim/extras/lang/nix.nix
@@ -18,7 +18,7 @@ in
 
   config = mkIf cfg.extras.lang.nix.enable {
     programs.neovim = {
-      extraPackages = builtins.attrValues { inherit (pkgs) nil nixfmt-rfc-style; };
+      extraPackages = builtins.attrValues { inherit (pkgs) nil; };
 
       plugins = [ (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: [ plugins.nix ])) ];
     };

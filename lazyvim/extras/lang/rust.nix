b01d9440ea1fb316f089a44ace47d873a973ceca
Jonathan Boyle <programmerfalcon@gmail.com>
2025-04-01 10:20:39 +0100

    Fix: no codelldb in nixpkgs!

--- lazyvim/extras/lang/rust.nix
+++ lazyvim/extras/lang/rust.nix
@@ -19,7 +19,7 @@ in
 
   config = mkIf cfg.extras.lang.rust.enable {
     programs.neovim = {
-      extraPackages = [pkgs.rust-analyzer] ++ optional cfg.extras.dap.core.enable (with pkgs; [codelldb cpptools]);
+      extraPackages = [pkgs.rust-analyzer] ++ optional cfg.extras.dap.core.enable (with pkgs; [lldb]);
 
       plugins =
         [

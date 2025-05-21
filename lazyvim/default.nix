4dea56bba3d6623320295e6cacd4af4ba351c6d5
Cameron Smith <cameron.ray.smith@gmail.com>
2025-05-21 19:21:50 -0400

    feat(lang): init python (#6)
    * fix(lazyvim): import `lang/python.nix` extra
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * feat(lang): init python
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * fix(python): add required plugins
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * fix(python): use venv-selector regexp branch
    
    produces error
    
    ```
    error: builder for '/nix/store/s89cjgfnyypkhx6qhh9l0fcr68xq5vz9-vimplugin-venv-selector.nvim-2024-09-15.drv' failed with exit code 1;
           last 25 log lines:
           > :Successfully required module: venv-selector.utils
           > Attempting to require module: venv-selector.user_commands
           > Entering Ex mode.  Type "visual" to go to Normal mode.
           > :Successfully required module: venv-selector.user_commands
           >
           > ======================================================
           > Require check succeeded for the following modules:
           >   - venv-selector.hooks
           >   - venv-selector.logger
           >   - venv-selector
           >   - venv-selector.venv
           >   - venv-selector.gui
           >   - venv-selector.path
           >   - venv-selector.config
           >   - venv-selector.search
           >   - venv-selector.workspace
           >   - venv-selector.utils
           >   - venv-selector.user_commands
           > All lua modules were checked.
           >
           > Require check failed for the following modules:
           >   - venv-selector.cached_venv
           >
           > Checkout https://nixos.org/manual/nixpkgs/unstable/#testing-neovim-plugins-neovim-require-check
           > ======================================================
           For full logs, run 'nix log /nix/store/s89cjgfnyypkhx6qhh9l0fcr68xq5vz9-vimplugin-venv-selector.nvim-2024-09-15.drv'.
    error: 1 dependencies of derivation '/nix/store/7mw136biclnv6bj9zz8zcgkbdlk58169-neovim-0.10.4.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/9yccjqm80zbziw6rzipxlsannfmp6897-home-manager-applications.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/i6sv5d8fx5nvfwilv1233zaif0rf74xg-home-manager-fonts.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/c07cx1xbbrl2im92yz58sciba6zs1nv8-home-manager-path.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/kq458jqldmakyjxiyvdy5lgmlys5qcrn-home-manager-generation.drv' failed to build
    error: 1 dependencies of derivation '/nix/store/j764xmcqy2lafxcl7qf4ld2wxffc3cqh-lazyvim-test-shell-env.drv' failed to build
    ```
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * fix(python): use venv-selector main branch
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * fix(python): use venv-selector regexp branch
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * fix(lang): supply ruff not ruff-lsp
    
    - https://www.github.com/LazyVim/LazyVim/pull/4126
    - https://github.com/LazyVim/LazyVim/blob/541b83276e0fe92f7d0d6c9f283cd15ccba5a2fd/lua/lazyvim/plugins/extras/lang/python.lua#L5-L6
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    
    * nitpicks
    
    * Remove the python TS plugin from the extra
    
    LazyVim installs it by default.
    
    * Remove venv-selector comment
    
    * Format with nixfmt
    
    * Remove debugpy from extraPackages
    
    * Add debugpy mason package
    
    * Change venv-selector.nvim to be optional
    
    * Change neotest-python to be optional
    
    * Change nvim-dap-python to be optional
    
    * Add attrValues inherit
    
    * Remove with expressions
    
    * Remove whitespace
    
    ---------
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>
    Co-authored-by: Daniel Mata <100956688+matadaniel@users.noreply.github.com>

--- lazyvim/default.nix
+++ lazyvim/default.nix
@@ -60,6 +60,7 @@ in
     ./extras/lang/markdown.nix
     ./extras/lang/nix.nix
     ./extras/lang/prisma.nix
+    ./extras/lang/python.nix
     ./extras/lang/svelte.nix
     ./extras/lang/tailwind.nix
     ./extras/lang/typescript.nix
@@ -119,7 +120,12 @@ in
       extraLuaConfig = ''
         ${
           let
-            inherit (cfg.extras.lang) astro svelte typescript;
+            inherit (cfg.extras.lang)
+              astro
+              python
+              svelte
+              typescript
+              ;
 
             selfPkgs = self.packages.${pkgs.stdenv.hostPlatform.system};
 
@@ -138,6 +144,15 @@ in
                 cond = (astro.enable || svelte.enable || typescript.enable) && cfg.extras.dap.core.enable;
                 name = "packages/js-debug-adapter/js-debug/src/dapDebugServer.js";
                 path = "${pkgs.vscode-js-debug}/lib/node_modules/js-debug/dist/src/dapDebugServer.js";
+              }
+              {
+                cond = python.enable && cfg.extras.dap.core.enable;
+                name =
+                  if pkgs.stdenv.hostPlatform.isWindows then
+                    "packages/debugpy/venv/Scripts/pythonw.exe"
+                  else
+                    "packages/debugpy/venv/bin/python";
+                path = (pkgs.python3.withPackages (ps: [ ps.debugpy ])).interpreter;
               }
             ];
 
---
+++ lazyvim/extras/lang/python.nix
@@ -0,0 +1,47 @@
+self:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (builtins) attrValues;
+  inherit (lib.lists) optional;
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.programs.lazyvim;
+in
+{
+  options.programs.lazyvim.extras.lang.python = {
+    enable = mkEnableOption "the lang.python extra";
+  };
+
+  config = mkIf cfg.extras.lang.python.enable {
+    programs.neovim = {
+      extraPackages = attrValues { inherit (pkgs) pyright ruff; };
+
+      plugins =
+        [
+          (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: attrValues { inherit (plugins) ninja rst; }))
+        ]
+        ++ optional cfg.extras.dap.core.enable pkgs.vimPlugins.nvim-dap-python
+        ++ optional cfg.extras.test.core.enable pkgs.vimPlugins.neotest-python;
+      # TODO: ++ optional cfg.extras.editor.telescope.enable (
+      #   pkgs.vimUtils.buildVimPlugin {
+      #     pname = "venv-selector.nvim";
+      #     version = "2024-09-15";
+      #     src = pkgs.fetchFromGitHub {
+      #       owner = "linux-cultist";
+      #       repo = "venv-selector.nvim";
+      #       rev = "e82594274bf7b54387f9a2abe65f74909ac66e97";
+      #       sha256 = "0d2lzx5b1jc0zq92ziy83apxak6b2rjsgi8nz6jwyy58l96lhb03";
+      #     };
+      #     meta.homepage = "https://github.com/linux-cultist/venv-selector.nvim/";
+      #     nvimSkipModule = [ "venv-selector.cached_venv" ];
+      #   }
+      # );
+    };
+  };
+}
